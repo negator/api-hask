2e54ced2809a24aeb55c45f9d9ae85af6dbc18f7
Naveen Gattu <naveen.gattu@gmail.com>
2016-03-23 14:45:40 -0700

    Filter cc associated bill transactions

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,13 @@ import Lib
 import Network.Wreq
 import qualified Network.Wreq.Session as S
 import Control.Lens
+import Control.Exception as E
 import Text.HTML.Scalpel
 import Text.HTML.TagSoup 
 import Data.Maybe
 import Data.List
 import Data.Char
+import Data.ByteString.Char8 (pack)
 import Data.ByteString.Lazy (ByteString)
 import Data.Aeson (toJSON)
 import System.Environment
@@ -25,23 +27,29 @@ main :: IO ()
 main = do 
     urls <- getArgs                            
     let url = headMay(urls) `getOrElse` def
-    r <- S.withSession $ \session -> S.getWith opts session url
-    let tags = parseTags(r ^.responseBody)
+    let header = unwords $ intersperse "\n" ofxheader 
+    let request = header ++ "\n\n" ++ xml
+    putStrLn request
+    r <- E.try(postWith opts url (pack request))
+    --let tags = parseTags(r ^.responseBody)
 
     --putStrLn "Username"
     --username <- getLine
     --putStrLn "Password"
     --passwd <- getLine
-    let f = (fromAttrib "action") `fmap` (filter (isTagOpenName "form") $ tags )
-    let i = (filter (isTagOpenName "input") $ tags )
-    let (hidden, is) = partition (isTagOpenName "hidden") i 
-    bs <- chase tags
-
-    print hidden
+    --let f = (fromAttrib "action") `fmap` (filter (isTagOpenName "form") $ tags )
+    --let i = (filter (isTagOpenName "input") $ tags )
+    --let (hidden, is) = partition (isTagOpenName "hidden") i 
+    --bs <- chase r
+    print request
+    --print (r ^. responseBody)
   where 
     def :: String
     def = "https://chaseonline.chase.com/"            
- 
+    
+    ofxheader = ["OFXHEADER:100","DATA:OFXSGML","VERSION:103","SECURITY:NONE","ENCODING:USASCII","CHARSET:1252","COMPRESSION:NONE","OLDFILEUID:NONE","NEWFILEUID:NONE"] 
+    xml = "<OFX><SIGNONMSGSRQV1><SONRQ><DTCLIENT>20160418021529.000[-8:PST]<USERID>test<USERPASS>test<LANGUAGE>ENG<FI><ORG>HAN<FID>5959</FI><APPID>QWIN<APPVER>0900</SONRQ></SIGNONMSGSRQV1></OFX>"
+    -- <BANKMSGSRQV1>    <STMTTRNRQ><TRNUID>23382938      <STMTRQ>        <BANKACCTFROM>          <BANKID>987654321          <ACCTID>23517          <ACCTTYPE>SAVINGS        </BANKACCTFROM>        <INCTRAN>          <INCLUDE>Y        </INCTRAN>      </STMTRQ>    </STMTTRNRQ>  </BANKMSGSRQV1></OFX>"
 
 
 
@@ -56,3 +64,4 @@ chase tags =  do
     forms =   htmls("form"::String)    
     inputs  = htmls(("form"::String) \\ ("input"::String))
     iframes  = htmls("iframe"::String)      
+
